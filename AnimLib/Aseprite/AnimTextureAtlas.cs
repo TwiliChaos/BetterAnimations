1088b01cc832c7ceff77c2fdc9fdb9fcc66b80c7
TwiliChaos <accounts+github@ilemni.com>
2024-07-15 00:48:34 -0700

    Use modified AsepriteDotNet dll Changes include making most public types' internal constructors instead be public

---
+++ AnimLib/Aseprite/AnimTextureAtlas.cs
@@ -0,0 +1,8 @@
+﻿using AsepriteDotNet;
+
+namespace AnimLib.Aseprite;
+
+public record AnimTextureAtlas(TextureRegion[] _regions, Asset<Texture2D> Texture) {
+  private readonly TextureRegion[] _regions = _regions;
+  public ReadOnlySpan<TextureRegion> Regions => _regions;
+}
