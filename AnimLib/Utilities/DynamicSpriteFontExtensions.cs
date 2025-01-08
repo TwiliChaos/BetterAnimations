f889390e0efbd91102d2f3d276848056b7bcc787
TwiliChaos <accounts+github@ilemni.com>
2025-01-07 19:15:03 -0800

    Implement non-allocation version of DrawString

--- AnimLib/Utilities/DynamicSpriteFontExtensions.cs
+++ AnimLib/Utilities/DynamicSpriteFontExtensions.cs
@@ -1,6 +1,6 @@
-using System.Collections;
 using System.Reflection;
 using ReLogic.Graphics;
+using SpriteCharacterData = ReLogic.Graphics.DynamicSpriteFont.SpriteCharacterData;
 
 namespace AnimLib.Utilities;
 
@@ -18,13 +18,152 @@ public static class DynamicSpriteFontExtensions {
   /// <param name="text">The text to draw.</param>
   /// <param name="position">The screen position to draw at.</param>
   /// <param name="color">The color of the text.</param>
-  public static void DrawString(this SpriteBatch spriteBatch, DynamicSpriteFont spriteFont, ReadOnlySpan<char> text,
-    Vector2 position, Color color) {
-    // TODO:
-    //   Avoid ToString, use span directly to avoid allocation
-    //   Requires PR in the tML repo, or unacceptably, copying decompiled code into mod code *and*
-    //   a bunch of reflection 
-    DynamicSpriteFontExtensionMethods.DrawString(spriteBatch, spriteFont, text.ToString(), position, color);
+  /// <param name="rotation">The rotation of the text.</param>
+  /// <param name="origin">The origin of the text.</param>
+  /// <param name="scale">The scale of the text.</param>
+  /// <param name="effects">The flip directions of the text.</param>
+  /// <param name="layerDepth">The depth of the text.</param>
+  public static void DrawString(this SpriteBatch spriteBatch,
+    DynamicSpriteFont spriteFont,
+    ReadOnlySpan<char> text,
+    Vector2 position,
+    Color color,
+    float rotation = 0,
+    Vector2 origin = default,
+    float scale = 1f,
+    SpriteEffects effects = SpriteEffects.None,
+    float layerDepth = 0) {
+    InternalDraw(spriteFont, text, spriteBatch, position, color, rotation,
+      origin, new Vector2(scale), effects, layerDepth);
   }
-}
 
+  private static void InternalDraw(this DynamicSpriteFont font,
+    ReadOnlySpan<char> text,
+    SpriteBatch spriteBatch,
+    Vector2 startPosition,
+    Color color,
+    float rotation,
+    Vector2 origin,
+    Vector2 scale,
+    SpriteEffects spriteEffects,
+    float depth) {
+    Matrix matrix =
+      Matrix.CreateTranslation(-origin.X * scale.X, -origin.Y * scale.Y, 0) *
+      Matrix.CreateRotationZ(rotation);
+    Vector2 charPosition = Vector2.Zero;
+    Vector2 flipDir = Vector2.One;
+    bool isNewLine = true;
+    float x = 0f;
+    if (spriteEffects != 0) {
+      Vector2 stringSize = font.MeasureString(text);
+      if (spriteEffects.HasFlag(SpriteEffects.FlipHorizontally)) {
+        x = stringSize.X * scale.X;
+        flipDir.X = -1f;
+      }
+
+      if (spriteEffects.HasFlag(SpriteEffects.FlipVertically)) {
+        charPosition.Y = (stringSize.Y - font.LineSpacing) * scale.Y;
+        flipDir.Y = -1f;
+      }
+    }
+
+    charPosition.X = x;
+    foreach (char c in text) {
+      switch (c) {
+        case '\n':
+          charPosition.X = x;
+          charPosition.Y += font.LineSpacing * scale.Y * flipDir.Y;
+          isNewLine = true;
+          continue;
+        case '\r':
+          continue;
+      }
+
+      SpriteCharacterData characterData = font.GetCharacterData(c);
+      Vector3 kerning = characterData.Kerning;
+      Rectangle padding = characterData.Padding;
+      if (spriteEffects.HasFlag(SpriteEffects.FlipHorizontally))
+        padding.X -= padding.Width;
+
+      if (spriteEffects.HasFlag(SpriteEffects.FlipVertically))
+        padding.Y = font.LineSpacing - characterData.Glyph.Height - padding.Y;
+
+      if (isNewLine)
+        kerning.X = Math.Max(kerning.X, 0f);
+      else
+        charPosition.X += font.CharacterSpacing * scale.X * flipDir.X;
+
+      charPosition.X += kerning.X * scale.X * flipDir.X;
+      Vector2 position = charPosition + padding.Size() * scale;
+      Vector2.Transform(ref position, ref matrix, out position);
+      position += startPosition;
+      spriteBatch.Draw(characterData.Texture, position, characterData.Glyph, color, rotation, Vector2.Zero, scale,
+        spriteEffects, depth);
+      charPosition.X += (kerning.Y + kerning.Z) * scale.X * flipDir.X;
+      isNewLine = false;
+    }
+  }
+
+  public static Vector2 MeasureString(this DynamicSpriteFont font, ReadOnlySpan<char> text) {
+    if (text.Length == 0)
+      return Vector2.Zero;
+
+    Vector2 size = Vector2.Zero;
+    size.Y = font.LineSpacing;
+    float xMax = 0f;
+    int lineCount = 0;
+    float kerningZ = 0f;
+    bool isNewLine = true;
+    foreach (char c in text) {
+      switch (c) {
+        case '\n':
+          xMax = Math.Max(size.X + Math.Max(kerningZ, 0f), xMax);
+          kerningZ = 0f;
+          size = Vector2.Zero;
+          size.Y = font.LineSpacing;
+          isNewLine = true;
+          lineCount++;
+          continue;
+        case '\r':
+          continue;
+      }
+
+      SpriteCharacterData characterData = font.GetCharacterData(c);
+      Vector3 kerning = characterData.Kerning;
+      if (isNewLine)
+        kerning.X = Math.Max(kerning.X, 0f);
+      else
+        size.X += font.CharacterSpacing + kerningZ;
+
+      size.X += kerning.X + kerning.Y;
+      kerningZ = kerning.Z;
+      size.Y = Math.Max(size.Y, characterData.Padding.Height);
+      isNewLine = false;
+    }
+
+    size.X += Math.Max(kerningZ, 0f);
+    size.Y += lineCount * font.LineSpacing;
+    size.X = Math.Max(size.X, xMax);
+    return size;
+  }
+
+  private static SpriteCharacterData GetCharacterData(this DynamicSpriteFont font, char character) {
+    var dict = font.GetSpriteCharacters();
+    return dict.TryGetValue(character, out SpriteCharacterData value) ? value : font.GetDefaultCharacterData();
+  }
+
+  private static Dictionary<char, SpriteCharacterData> GetSpriteCharacters(this DynamicSpriteFont font) {
+    return (Dictionary<char, SpriteCharacterData>)SpriteCharactersField.GetValue(font)!;
+  }
+
+  private static SpriteCharacterData GetDefaultCharacterData(this DynamicSpriteFont font) {
+    return (SpriteCharacterData)DefaultCharacterDataField.GetValue(font)!;
+  }
+
+  private static readonly FieldInfo SpriteCharactersField = Field("_spriteCharacters");
+
+  private static readonly FieldInfo DefaultCharacterDataField = Field("_defaultCharacterData");
+
+  private static FieldInfo Field(string field) =>
+    typeof(DynamicSpriteFont).GetField(field, BindingFlags.NonPublic | BindingFlags.Instance)!;
+}
